d9e835c0c49dc4d60e7284299f9c9a59b27a3e11
Erin Timlin <erintimlin@gmail.com>
2023-06-14 17:23:47 -0400

    add inital files for google oauth

--- customer-portal/app/page.tsx
+++ customer-portal/app/page.tsx
@@ -1,16 +1,17 @@
 import TrelloCard  from "../utility/TrelloCard"
 import ParseBoard from "@/utility/ParseBoard";
+import Login from './Login'
 
 export default async function Home() {
 
-  const res = await fetch("",   { cache : 'no-store'}
-  );
-  const data = await res.json();
-  console.log(data)
+  // const res = await fetch("",   { cache : 'no-store'}
+  // );
+  // const data = await res.json();
+  // console.log(data)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello world</h1>
-      <h2>{data[0].name}</h2>
+      <Login />
     </main>
   )
 }
