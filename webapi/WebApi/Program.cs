93b38cbb22531b1ffb8c861f28d8fc1cd5f9f59d
Zookerman3 <szook7@gmail.com>
2023-06-28 13:52:17 -0500

    edited files, deleted jwt authentication in program.cs

--- webapi/WebApi/Program.cs
+++ webapi/WebApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using WebApi.DataAccess;
-using WebApi.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -15,64 +22,38 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient();
 
-builder.Services.AddIdentity<AppUser, IdentityRole>()
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(options =>
+{
+    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
+builder.Services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddSignInManager()
     .AddEntityFrameworkStores<PostgreSqlContext>()
     .AddDefaultTokenProviders();
 
-// Adding Authentication
-builder.Services.AddAuthentication(options =>
-{
-  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-  options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-});
-// Adding Jwt Bearer to Identity
-// .AddJwtBearer(options =>
-// {
-//   options.SaveToken = true;
-//   options.RequireHttpsMetadata = false;
-//   options.TokenValidationParameters = new TokenValidationParameters()
-//   {
-//     ValidateIssuer = true,
-//     ValidateAudience = true,
-//     ValidAudience = configuration["JWT:ValidAudience"],
-//     ValidIssuer = configuration["JWT:ValidIssuer"],
-//     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
-//   };
-// });
-
-// builder.Services.AddAuthentication(options =>
-// {
-//     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-//     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-// })
-// .AddCookie()
-// .AddGoogle(options =>
-// {
-//     options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-//     options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-// });
-
-// builder.Services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//     .AddSignInManager() 
-//     .AddEntityFrameworkStores<PostgreSqlContext>()
-//     .AddDefaultTokenProviders();
-
-var sqlConnectionString = configuration["PostgreSqlConnectionString"]; 
-builder.Services.AddDbContext<PostgreSqlContext>(options => options.UseNpgsql(sqlConnectionString));  
-// builder.Services.AddScoped<IUserDataAccessProvider, UserDataAccessProvider>(); 
+var sqlConnectionString = configuration["PostgreSqlConnectionString"];
+builder.Services.AddDbContext<PostgreSqlContext>(options => options.UseNpgsql(sqlConnectionString));
+builder.Services.AddScoped<IDataAccessProvider, DataAccessProvider>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 }
 
 app.UseAuthentication();
@@ -81,3 +62,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
