ef42db87c4a8f8be7cf4ec7eb748d076d8c47e2f
Zookerman3 <szook7@gmail.com>
2023-06-28 21:58:49 -0500

    trello board api call working successfully

--- webapi/WebApi/Controllers/TrelloController.cs
+++ webapi/WebApi/Controllers/TrelloController.cs
@@ -20,8 +20,10 @@ namespace WebApi.Controllers
         public async Task<IActionResult> GetTrelloBoard(string cardId, string apikey, string apitoken)
         {
             var request = new HttpRequestMessage(HttpMethod.Get,
-            $"https://api.trello.com/1/cards/{cardId}/attachments?key={apikey}&token={apitoken}");
+            $"https://api.trello.com/1/boards/{cardId}?key={apikey}&token={apitoken}");
             request.Headers.Add("Accept", "application/vnd.github.v3+json");
+
+// https://api.trello.com/1/boards/64823e0ae015b3e2013c7f6b?key=fe91479f8115fc056c49911580effd95&token=ATTA6cff2e9c9ca4b4b103bb93ed6180651b0cb9a138113d3e73da4fc9f2bf646d7c9B79F36B
 
             var client = _clientFactory.CreateClient();
 
