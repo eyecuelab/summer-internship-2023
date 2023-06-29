f976f464422a5a5067c9f3d34040b65370c15156
Eliot Gronstal <eliot.lauren@gmail.com>
2023-06-28 20:03:34 -0700

    update branch.

--- customer-portal/app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Admin() {
-  return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
-  )
-}
--- customer-portal/app/user/details/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Detail() {
-  return (
-    <div>
-      <h1>Ticket Detail Page</h1>
-    </div>
-  )
-}
