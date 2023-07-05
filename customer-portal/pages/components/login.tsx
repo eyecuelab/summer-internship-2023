4e37812eb3d4d8fd6c518f1210d6b28263817686
Zookerman3 <szook7@gmail.com>
2023-07-05 05:56:08 -0500

    wip cant get site to run

--- customer-portal/pages/components/login.tsx
+++ customer-portal/pages/components/login.tsx
@@ -1,25 +1,25 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Session } from "next-auth";
-import Link from 'next/link'; // Import Session type from 'next-auth'
+import Link from "next/link"; // Import Session type from 'next-auth'
 
 export default function Login() {
-  const { data: session } = useSession();
-  if (session) {
+    const { data: session } = useSession();
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user?.email} <br />
+                Hello {session.user?.name} <br />
+                {/* Your Profile Icon is: {session.user?.image && <img src={session.user?.image} width={50} />} <br /> */}
+                <br />
+                <Link href="/components/restricted"></Link>
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        );
+    }
     return (
-      <>
-        Signed in as {session.user?.email} <br />
-        Hello {session.user?.name} <br />
-        {/* Your Profile Icon is: {session.user?.image && <img src={session.user?.image} width={50} />} <br /> */}
-
-        Check out the <a href="/components/restricted">examples API route</a> <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+        </>
     );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  );
 }
