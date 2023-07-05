d31ba9cd9a3204cf48d098fa3f6a8b51fc052db5
Zookerman3 <szook7@gmail.com>
2023-07-05 05:25:45 -0500

    wip

--- customer-portal/server.js
+++ customer-portal/server.js
@@ -1,37 +1,27 @@
 const express = require('express');
-const next = require('next');
+const axios = require('axios');
 const https = require('https');
+const cors = require('cors');
 
-const dev = process.env.NODE_ENV !== 'production';
-const nextApp = next({ dev });
-const handle = nextApp.getRequestHandler();
+const app = express();
 
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
+app.use(cors());  // <-- This will enable CORS for all routes
+
+const agent = new https.Agent({
+  rejectUnauthorized: false
 });
 
-nextApp.prepare().then(() => {
-  const app = express();
+app.get('/api/commits', async (req, res) => {
+  try {
+    const response = await axios.get('https://localhost:7243/api/GitHub/commits/eyecuelab/summer-internship-2023', { httpsAgent: agent });
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  app.get('/api/GitHub/commits/:org/:repo', async (req, res) => {
-    const url = `https://localhost:7243/api/GitHub/commits/${req.params.org}/${req.params.repo}`;
-    try {
-        const { default: fetch } = await import('node-fetch');
-      const apiRes = await fetch(url, { agent: httpsAgent });
-      const apiResJson = await apiRes.json();
-      res.json(apiResJson);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while trying to fetch data from the API' });
-    }
-  });
-
-  app.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  app.listen(3000, err => {
-    if (err) throw err;
-    console.log('Server ready on http://localhost:3000');
-  });
+app.listen(4000, () => {
+  console.log('Server running on port 4000');
 });
