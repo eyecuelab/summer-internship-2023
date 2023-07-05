e6c0285f30fb6c4eb6c33737b4d93b281fe22c08
Zookerman3 <szook7@gmail.com>
2023-07-05 16:59:12 -0500

    IT ACTUALLY WORKS!!!

--- customer-portal/pages/api/auth/[...nextauth].tsx
+++ customer-portal/pages/api/auth/[...nextauth].tsx
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session, User } from "next-auth";
 import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
 import GoogleProvider from "next-auth/providers/google";
+import { JWT } from "next-auth/jwt";
+import { AdapterUser } from "next-auth/adapters";
 
 export default NextAuth({
     providers: [
--- customer-portal/pages/components/restricted.tsx
+++ customer-portal/pages/components/restricted.tsx
@@ -1,80 +1,104 @@
 import React, { useState, useEffect, PropsWithChildren } from "react";
-import { useSession, signOut, getSession, GetSessionParams } from "next-auth/react";
+import {
+    useSession,
+    signOut,
+    getSession,
+    GetSessionParams,
+} from "next-auth/react";
+import classNames from "classnames";
 import Sidebar from "./sidebar";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import Layout from "./layout";
+import { Session } from "next-auth";
+import axios from "axios";
 
 interface Commit {
-  name: string;
-  message: string;
-  date: string;
+    name: string;
+    message: string;
+    date: string;
 }
 
 interface CommitResponse {
-  commit: {
-    author: {
-      name: string;
-      email: string;
-      date: string;
+    commit: {
+        author: {
+            name: string;
+            email: string;
+            date: string;
+        };
+        message: string;
     };
-    message: string;
-  };
+}
+
+async function register(session: Session | null) {
+    try {
+        await axios.post("https://localhost:7243/api/users/register", session?.user);
+    } catch (error) {
+        console.error("Failed to transmit user data:", error);
+    }
 }
 
 const Restricted = () => {
-  const { data: session, status } = useSession({ required: true });
-  const [apiData, setApiData] = useState<Commit[] | null>(null);
+    const { data: session, status } = useSession({ required: true });
+    const [apiData, setApiData] = useState<Commit[] | null>(null);
 
-  useEffect(() => {
-    if (status === "authenticated") {
-      fetch("http://localhost:4000/api/commits")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("HTTP error, status = " + response.status);
-          }
-          return response.json();
-        })
-        .then((json: CommitResponse[]) => {
-          const commits = json.map(commit => ({
-            name: commit.commit.author.name,
-            message: commit.commit.message,
-            date: commit.commit.author.date
-          }));
-          setApiData(commits);
-        })
-        .catch((error) => console.error("Error during fetch:", error));
-    }
-  }, [status]);
+    useEffect(() => {
+        if (status === "authenticated") {
 
-  return status === "authenticated" ? (
-    <Layout username={session?.user?.name}>
-      <p>Commit Messages:</p>
-      {apiData && apiData.map((commit, index) => (
-        <div key={index}>
-          <p>Name: {commit.name}</p>
-          <p>Message: {commit.message}</p>
-          <p>Date: {commit.date}</p>
-        </div>
-      ))}
-    </Layout>
-  ) : (
-    <div>loading...</div>
-  );
+            register(session);
+            console.log(session);
+            fetch("http://localhost:4000/api/commits")
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error(
+                            "HTTP error, status = " + response.status
+                        );
+                    }
+                    return response.json();
+                })
+                .then((json: CommitResponse[]) => {
+                    const commits = json.map((commit) => ({
+                        name: commit.commit.author.name,
+                        message: commit.commit.message,
+                        date: commit.commit.author.date,
+                    }));
+                    setApiData(commits);
+                })
+                .catch((error) => console.error("Error during fetch:", error));
+        }
+    }, [status]);
+
+    return status === "authenticated" ? (
+        <Layout username={session?.user?.name}>
+            <p>Commit Messages:</p>
+            {apiData &&
+                apiData.map((commit, index) => (
+                    <div key={index}>
+                        <p>Name: {commit.name}</p>
+                        <p>Message: {commit.message}</p>
+                        <p>Date: {commit.date}</p>
+                    </div>
+                ))}
+        </Layout>
+    ) : (
+        <div>loading...</div>
+    );
 };
 
 export default Restricted;
 
-export async function getServerSideProps(context: GetSessionParams | undefined) {
-  const session = await getSession(context);
-  if (!session) {
+export async function getServerSideProps(
+    context: GetSessionParams | undefined
+) {
+    const session = await getSession(context);
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
     return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
+        props: { session },
     };
-  }
-  return {
-    props: { session },
-  };
 }
