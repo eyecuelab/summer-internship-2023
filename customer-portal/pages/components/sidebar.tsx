f762dfe083678716bdf1f5459f960d58162de76f
Brishna Bakshev <b.bakshev@gmail.com>
2023-07-11 13:06:51 -0700

    side bar is displaying user info from github repo

--- customer-portal/pages/components/sidebar.tsx
+++ customer-portal/pages/components/sidebar.tsx
@@ -14,34 +14,34 @@ import ProfileSidebar from "./ProfileSidebar";
 type Props = {
   collapsed: boolean;
   setCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
-  setSelectedUser: React.Dispatch<React.SetStateAction<string | null>>;
 };
-const Sidebar = ({ collapsed, setCollapsed, setSelectedUser }: Props) => {
-  const [users, setUsers] = useState<any[]>([]);
-  const [currentPage, setCurrentPage] = useState<string>(''); // Track the current page
+const Sidebar = ({ collapsed, setCollapsed }: Props) => {
+  const [authors, setAuthors] = useState<any[]>([]);
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState<string>('');
 
   useEffect(() => {
-    setCurrentPage(router.pathname); // Set the current page on initial load and on route changes
+    setCurrentPage(router.pathname);
   }, [router.pathname]);
 
-      // Function to fetch users
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchCommits = async () => {
       try {
-        const response = await fetch("https://localhost:7243/api/Users"); 
+        const response = await fetch("https://localhost:7243/api/GitHub/commits/eyecuelab/summer-internship-2023"); 
         const data = await response.json();
-        setUsers(data);
+        // Extracting unique authors
+        const uniqueAuthors = Array.from(new Set(data.map((commit: any) => commit.commit.author.name)));
+        setAuthors(uniqueAuthors);
       } catch (error) {
-        console.error('Failed to fetch users:', error);
+        console.error('Failed to fetch commits:', error);
       }
     };
 
-    fetchUsers();
-  }, []);
+    fetchCommits();
+  }, []);   
 
-    // 👇 use the correct icon depending on the state.
   const Icon = collapsed ? ChevronDoubleRightIcon : ChevronDoubleLeftIcon;
+  
   return (
     <div className={cn({"bg-gray-100 text-black-50 z-20": true})}>
       <div className={cn({"flex flex-col justify-between": true})}>
@@ -61,30 +61,23 @@ const Sidebar = ({ collapsed, setCollapsed, setSelectedUser }: Props) => {
             })}
             onClick={() => setCollapsed(!collapsed)}
           >
-            <Icon className="w-5 h-5" />
+            <Icon className="w-5 h-5" /> 
           </button>
         </div>
       </div>
       <div className={cn({ "grid place-content-stretch p-4 ": true })}>
-      {currentPage.startsWith('/Profile') ? ( // Check if the current page starts with '/Profile'
+      {currentPage.startsWith('/Profile') ? (
         <ProfileSidebar collapsed={collapsed} setCollapsed={setCollapsed} />
       ) : (
-        users.map((user) => (
-            <div key={user.id} className="flex gap-4 items-center h-11 overflow-hidden bg-gray-200 hover:bg-gray-400 rounded-full">
-              <Image
-                src={user.image}
-                height={36}
-                width={36}
-                alt="profile image"
-                className="rounded-full"
-              />
+        authors.map((author, index) => (
+            <div key={index} className="flex gap-4 items-center h-11 overflow-hidden bg-gray-200 hover:bg-gray-400 rounded-full">
               {!collapsed && (
                 <div className="flex flex-col">
                   <Link
-                    href={`/Profile/${user.id}`} // Pass the user ID to the profile route
+                    href={`/Profile/${author}`} // Pass the author name to the profile route
                     className="text-slate-500 text-sm"
                   >
-                    {user.email}
+                    {author}
                   </Link>
                 </div>
               )}
@@ -97,8 +90,3 @@ const Sidebar = ({ collapsed, setCollapsed, setSelectedUser }: Props) => {
 };
 
 export default Sidebar;
-
-
-
-
-
