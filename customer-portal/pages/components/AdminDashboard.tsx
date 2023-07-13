ebbe1c570fa13b45bb71af27edb9d4902db5a804
Zookerman3 <szook7@gmail.com>
2023-07-13 12:03:32 -0500

    wip

--- customer-portal/pages/components/AdminDashboard.tsx
+++ customer-portal/pages/components/AdminDashboard.tsx
@@ -191,7 +191,7 @@ const AdminDashboard = () => {
         };
 
         fetchAllUsersForProject();
-    }, [currentProject]);
+    }, [intialProject]);
 
     return status === "authenticated" ? (
         <AdminLayout
