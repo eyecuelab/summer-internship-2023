1d4f0d748a5b1b3bd53d4df96e552ed8614c85ba
Eliot Gronstal <eliot.lauren@gmail.com>
2023-07-15 11:39:21 -0700

    update AdminProfile with Fab and AddIcon stylings. Update AdminDashboard project contributor link to display selected User's email address at the top of AdminProfile.

--- customer-portal/pages/components/AddProjectsModal.tsx
+++ customer-portal/pages/components/AddProjectsModal.tsx
@@ -3,103 +3,103 @@ import { Modal, Button, Text, Input, Row } from "@nextui-org/react";
 import axios from "axios";
 
 interface ProjectModalProps {
-  currentEntity: Entity | null;
-  onSelectedEntity: (selectedEntity: Entity | null) => void;
+    currentEntity: Entity | null;
+    onSelectedEntity: (selectedEntity: Entity | null) => void;
 }
 
 interface Entity {
-  companyName: string;
-  entityId: string;
+    companyName: string;
+    entityId: string;
 }
 
 export default function AddProjectModal({
-  currentEntity,
-  onSelectedEntity,
+    currentEntity,
+    onSelectedEntity,
 }: ProjectModalProps) {
-  const [visible, setVisible] = useState(false);
-  const [projectName, setProjectName] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [projectName, setProjectName] = useState("");
 
-  const handleOpen = () => setVisible(true);
-  const handleClose = () => setVisible(false);
+    const handleOpen = () => setVisible(true);
+    const handleClose = () => setVisible(false);
 
-  console.log("current entity in projectmodal ", currentEntity);
+    console.log("current entity in projectmodal ", currentEntity);
 
-  const handleAddProject = async () => {
-    try {
-      const url = "https://localhost:7243/api/projects";
-      const emailEntityPayload = {
-        EntityId: currentEntity?.entityId,
-        ProjectName: projectName,
-      };
+    const handleAddProject = async () => {
+        try {
+            const url = "https://localhost:7243/api/projects";
+            const emailEntityPayload = {
+                EntityId: currentEntity?.entityId,
+                ProjectName: projectName,
+            };
 
-      const response = await axios.post(url, emailEntityPayload);
+            const response = await axios.post(url, emailEntityPayload);
 
-      // Handle the response data
-      console.log(response.data);
-    } catch (error) {
-      // Handle error
-      console.error(error);
-    }
+            // Handle the response data
+            console.log(response.data);
+        } catch (error) {
+            // Handle error
+            console.error(error);
+        }
 
-    // Reset input fields
-    setProjectName("");
+        // Reset input fields
+        setProjectName("");
 
-    // Close the modal
-    handleClose();
-  };
+        // Close the modal
+        handleClose();
+    };
 
-  return (
-    <div>
-      <Button
-        shadow
-        onPress={handleOpen}
-        style={{
-          borderRadius: "999px",
-          backgroundColor: "#E6E6E6",
-          color: "#404040",
-          padding: "8px 12px",
-          fontFamily: "Rasa",
-          fontSize: "20px",
-          cursor: "pointer",
-        }}
-      >
-        Add Project
-      </Button>
-      <Modal
-        open={visible}
-        onClose={handleClose}
-        aria-labelledby="modal-title"
-        closeButton
-      >
-        <Modal.Header>
-          <Text id="modal-title" style={projectStyleInter} size={18}>
-            Add Project
-          </Text>
-        </Modal.Header>
-        <Modal.Body>
-          <Input
-            clearable
-            bordered
-            fullWidth
-            color="primary"
-            size="lg"
-            placeholder="Project Name"
-            value={projectName}
-            onChange={(e) => setProjectName(e.target.value)}
-            style={projectNameStyle}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Row justify="flex-end">
-            <Button style={closeProjectStyle} onPress={handleClose}>
-              Close
+    return (
+        <div>
+            <Button shadow onPress={handleOpen}               
+            style={{
+                borderRadius: "999px",
+                backgroundColor: "#E6E6E6",
+                color: "#404040",
+                padding: "8px 12px",
+                fontFamily: "Rasa",
+                fontSize: "20px",
+                cursor: "pointer",
+            }}
+            >
+                Add Project
             </Button>
-            <Button style={addProjectStyle} onPress={handleAddProject}>
-              Add
-            </Button>
-          </Row>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+            <Modal
+                open={visible}
+                onClose={handleClose}
+                aria-labelledby="modal-title"
+                closeButton
+            >
+                <Modal.Header>
+                    <Text id="modal-title"
+                    size={18}>
+                        Add Project
+                    </Text>
+                </Modal.Header>
+                <Modal.Body>
+                    <Input
+                        clearable
+                        bordered
+                        fullWidth
+                        color="primary"
+                        size="lg"
+                        placeholder="Project Name"
+                        value={projectName}
+                        onChange={(e) => setProjectName(e.target.value)}
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Row justify="flex-end">
+                        <Button onPress={handleClose}>
+                            Close
+                        </Button>
+                        <Button
+                            onPress={handleAddProject}
+                        >
+                            Add
+                        </Button>
+                    </Row>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
