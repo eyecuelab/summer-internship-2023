4ed7f4a50881af234edacdc28aefe829cb62d4c2
Brishna Bakshev <b.bakshev@gmail.com>
2023-07-18 11:29:27 -0700

    Changed the code to GET

--- customer-portal/pages/components/ReleaseNotes.tsx
+++ customer-portal/pages/components/ReleaseNotes.tsx
@@ -3,27 +3,42 @@ import axios from "axios";
 
 const ReleaseNotes: React.FC = () => {
   const [releaseNotes, setReleaseNotes] = useState<string>("");
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
   const [date, setDate] = useState<string>("");
 
-  // Function to handle date input change
-  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDate(event.target.value);
+
+  // Functions to handle date input changes
+  const handleStartDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDate(event.target.value);
+    console.log(event.target.value);
   };
 
   // Function to handle button click
   const handleReleaseNotesClick = async () => {
-    try {
-      const response = await axios.post(`http://localhost:7243/api/OpenAI/SummarizeCommitsByDates/${date}/${date}`);
-      setReleaseNotes(response.data);
-    } catch (error) {
-      console.error("Failed to fetch release notes:", error);
+    console.log(startDate, endDate);
+    if (startDate !== "" && endDate !== "") {
+      try {
+        const response = await axios.get(`http://localhost:7243/api/OpenAI/SummarizeCommitsByDates`, { params: { startDate: startDate, endDate: endDate } });
+        setReleaseNotes(response.data);
+      } catch (error) {
+        console.error("Failed to fetch release notes:", error);
+      }
+    } else {
+      console.error("Start date or end date is not set");
     }
   };
+  
 
   return (
     <div>
       <h2>Release Notes</h2>
-      <input type="date" value={date} onChange={handleDateChange} />
+      <input type="date" value={startDate} onChange={handleStartDateChange} />
+      <input type="date" value={endDate} onChange={handleEndDateChange} />
       <button onClick={handleReleaseNotesClick}>Get Release Notes</button>
       <p>{releaseNotes}</p>
     </div>
