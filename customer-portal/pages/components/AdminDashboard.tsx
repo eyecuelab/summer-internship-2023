5d194c58c03f44727f7fb1e927e41e62cb662373
Zookerman3 <szook7@gmail.com>
2023-07-19 17:36:37 -0500

    fixed userouter

--- customer-portal/pages/components/AdminDashboard.tsx
+++ customer-portal/pages/components/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSession, getSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import useRouter from "next/router";
 import AdminLayout from "./adminLayout";
 import { Session } from "next-auth";
 import { GetServerSidePropsContext } from "next";
--- customer-portal/pages/components/sidebar.tsx
+++ customer-portal/pages/components/sidebar.tsx
@@ -6,7 +6,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import useRouter from "next/router";
 import ProfileSidebar from "./ProfileSidebar";
 import SelectedUserContext from "../context/selectedUserContext";
 
