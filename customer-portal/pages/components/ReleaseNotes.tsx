ab09292a3972043039237e72a06a92ad05180a01
Zookerman3 <szook7@gmail.com>
2023-07-19 18:54:25 -0500

    formatdate

--- customer-portal/pages/components/ReleaseNotes.tsx
+++ customer-portal/pages/components/ReleaseNotes.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
+import { formatDate } from './TrelloSprint';
 
 type ReleaseNote = {
   endDate: string;
