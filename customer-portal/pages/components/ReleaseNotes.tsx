d9ac0f150c31b98b3222eacfa47b547c7a3fe1d3
Brishna Bakshev <b.bakshev@gmail.com>
2023-07-19 16:04:57 -0700

    updates to trello and releasenotes

--- customer-portal/pages/components/ReleaseNotes.tsx
+++ customer-portal/pages/components/ReleaseNotes.tsx
@@ -1,7 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
-import { formatDate } from "./TrelloSprint";
+
+type ReleaseNote = {
+  endDate: string;
+  responseId: string;
+  responseText: string;
+  startDate: string;
+};
 
 type ReleaseNotesProps = {
   startDate: string;
@@ -9,16 +15,16 @@ type ReleaseNotesProps = {
 };
 
 const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
-  const [sections, setSections] = useState<Array<string>>([]);
+  const [sections, setSections] = useState<string[]>([]);
 
   useEffect(() => {
     const handleReleaseNotesFetch = async () => {
       try {
-        const formattedStartDate = startDate ? formatDate(new Date(startDate)) : '';
-        const formattedEndDate = endDate ? formatDate(new Date(endDate)) : '';
+        const formattedStartDate = new Date(startDate).toISOString().split('T')[0];
+        const formattedEndDate = new Date(endDate).toISOString().split('T')[0];
     
-        const response = await axios.get(
-          "https://localhost:7243/api/OpenAI/responses",
+        const response = await axios.get<ReleaseNote[]>(
+          'https://localhost:7243/api/OpenAI/responses',
           {
             params: {
               startDate: formattedStartDate,
@@ -27,7 +33,7 @@ const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
           }
         );
     
-        console.log("Fetched release notes: ", response.data);
+        console.log('Fetched release notes: ', response.data);
     
         if (response.data && response.data.length > 0) {
           const filteredResponse = response.data[0];
@@ -41,26 +47,24 @@ const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
           setSections([]);
         }
       } catch (error) {
-        console.log("Error fetching data:", error);
+        console.log('Error fetching data:', error);
       }
     };
+    
 
-    if (startDate !== "" && endDate !== "") {
+    if (startDate !== '' && endDate !== '') {
       handleReleaseNotesFetch();
     }
   }, [startDate, endDate]);
 
-  console.log("Sections: ", sections);
-  console.log("Sections length: ", sections.length);
-
   return (
     <div className="text-gray-500">
-      <h2 className="text-xl font-semibold text-gray-600" style={{ fontFamily: 'Rasa, sans-serif' }}>Release Notes</h2>
+      <h2 className="text-xl font-semibold text-gray-600" style={{ fontFamily: 'Rasa, sans-serif' }}>
+        Release Notes
+      </h2>
       <div className="prose max-w-none mt-4">
         {sections.length > 0 ? (
-          sections.map((section, index) => (
-            <ReactMarkdown key={index} children={section} />
-          ))
+          sections.map((section, index) => <ReactMarkdown key={index} children={section} />)
         ) : (
           <p>No release notes for the selected dates.</p>
         )}
