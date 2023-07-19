c88e00b3f26bb4a20bbd22342a9f83a77e0c4d1c
Zookerman3 <szook7@gmail.com>
2023-07-19 17:33:55 -0500

    fixed releasenotes

--- customer-portal/pages/components/ReleaseNotes.tsx
+++ customer-portal/pages/components/ReleaseNotes.tsx
@@ -3,39 +3,33 @@ import axios from "axios";
 import ReactMarkdown from 'react-markdown';
 import { formatDate } from "./TrelloSprint";
 
-
 interface Commit {
-    message: string;
+  message: string;
+  date: string;
+  author: {
+    name: any;
+    email: string;
     date: string;
-    author: {
-      name: any;
-      email: string;
-      date: string;
-    };
-  }
+  };
+}
 
-  interface ReleaseNotesProps {
-    latestCommits: Commit[];
-    setLatestCommits: (commits: Commit[]) => void;
-    startDate: string;
-     endDate: string;
-  }
+interface ReleaseNotesProps {
+  latestCommits: Commit[];
+  setLatestCommits: (commits: Commit[]) => void;
+}
 
-  const ReleaseNotes = ({ latestCommits, setLatestCommits }: ReleaseNotesProps) => {
+const ReleaseNotes = ({ latestCommits, setLatestCommits }: ReleaseNotesProps) => {
   const [sections, setSections] = useState<Array<string>>([]);
   const [startDate, setStartDate] = useState<string>("");
   const [endDate, setEndDate] = useState<string>("");
 
-
-  const handleStartDateChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
+  const handleStartDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setStartDate(event.target.value);
   };
 
-const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
-  const [sections, setSections] = useState<Array<string>>([]);
-
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDate(event.target.value);
+  };
 
   const handleGetLatestCommits = async () => {
     try {
@@ -44,74 +38,61 @@ const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
         throw new Error("HTTP error, status = " + response.status);
       }
       const json = await response.json();
-      console.log(json);
       const commits = json.map((commit: { message: any; date: any; author: { name: any; email: any; date: any; }; }) => ({
         message: commit.message,
         date: commit.date,
         releaseNotes: "",
         author: {
-            name: commit?.author?.name || "",
-            email: commit?.author?.email || "",
-            date: commit?.author?.date || "",
+          name: commit?.author?.name || "",
+          email: commit?.author?.email || "",
+          date: commit?.author?.date || "",
         },
       }));
 
       setLatestCommits(commits);
-      // Pass the commits to the parent component
-      if (setLatestCommits) {
-        setLatestCommits(commits);
-      }
     } catch (error) {
       console.error("Error during fetch:", error);
     }
   };
-  
 
+  const handleReleaseNotesFetch = async () => {
+    try {
+      const formattedStartDate = startDate ? formatDate(new Date(startDate)) : '';
+      const formattedEndDate = endDate ? formatDate(new Date(endDate)) : '';
 
-  const handleReleaseNotesClick = async () => {
-    if (startDate !== "" && endDate !== "") {
-  useEffect(() => {
-    const handleReleaseNotesFetch = async () => {
+      const response = await axios.get(
+        "https://localhost:7243/api/OpenAI/responses",
+        {
+          params: {
+            startDate: formattedStartDate,
+            endDate: formattedEndDate,
+          },
+        }
+      );
 
-      try {
-        const formattedStartDate = startDate ? formatDate(new Date(startDate)) : '';
-        const formattedEndDate = endDate ? formatDate(new Date(endDate)) : '';
-    
-        const response = await axios.get(
-          "https://localhost:7243/api/OpenAI/responses",
-          {
-            params: {
-              startDate: formattedStartDate,
-              endDate: formattedEndDate,
-            },
-          }
-        );
-    
-        console.log("Fetched release notes: ", response.data);
-    
-        if (response.data && response.data.length > 0) {
-          const filteredResponse = response.data[0];
-          if (filteredResponse) {
-            const sections = filteredResponse.responseText.split('\n\n');
-            setSections(sections);
-          } else {
-            setSections([]);
-          }
+      console.log("Fetched release notes: ", response.data);
+
+      if (response.data && response.data.length > 0) {
+        const filteredResponse = response.data[0];
+        if (filteredResponse) {
+          const sections = filteredResponse.responseText.split('\n\n');
+          setSections(sections);
         } else {
           setSections([]);
         }
-      } catch (error) {
-        console.log("Error fetching data:", error);
+      } else {
+        setSections([]);
       }
-    };
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
 
+  useEffect(() => {
     if (startDate !== "" && endDate !== "") {
       handleReleaseNotesFetch();
     }
   }, [startDate, endDate]);
-
-  console.log("Sections: ", sections);
-  console.log("Sections length: ", sections.length);
 
   return (
     <div className="text-gray-500">
@@ -130,14 +111,14 @@ const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
         className="py-2 px-4 mt-2 ml-2 rounded-lg border border-gray-200 focus:outline-none focus:border-blue-400"
       />
       <button
-        onClick={handleReleaseNotesClick}
-        className="py-2 px-4 mt-2 ml-2 bg-gray-200 hover:bg-gray-400  text-slate-500 rounded-lg focus:outline-none"
+        onClick={handleReleaseNotesFetch}
+        className="py-2 px-4 mt-2 ml-2 bg-gray-200 hover:bg-gray-400 text-slate-500 rounded-lg focus:outline-none"
       >
         Get Release Notes
       </button>
       <button
         onClick={handleGetLatestCommits}
-        className="py-2 px-4 mt-2 ml-2 bg-gray-200 hover:bg-gray-400  text-slate-500 rounded-lg focus:outline-none"
+        className="py-2 px-4 mt-2 ml-2 bg-gray-200 hover:bg-gray-400 text-slate-500 rounded-lg focus:outline-none"
       >
         Get Latest Commits
       </button>
