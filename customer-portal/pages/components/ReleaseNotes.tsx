dbfa08b7ca6c091f2dd09a604601bc6b4790f51e
Brishna Bakshev <b.bakshev@gmail.com>
2023-07-19 15:04:32 -0700

    Trello and Release Notes are working

--- customer-portal/pages/components/ReleaseNotes.tsx
+++ customer-portal/pages/components/ReleaseNotes.tsx
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ReactMarkdown from 'react-markdown';
+import { formatDate } from "./TrelloSprint";
 
-const ReleaseNotes: React.FC = () => {
+type ReleaseNotesProps = {
+  startDate: string;
+  endDate: string;
+};
+
+const ReleaseNotes: React.FC<ReleaseNotesProps> = ({ startDate, endDate }) => {
   const [sections, setSections] = useState<Array<string>>([]);
-  const [startDate, setStartDate] = useState<string>("");
-  const [endDate, setEndDate] = useState<string>("");
 
-  const handleStartDateChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setStartDate(event.target.value);
-  };
-
-  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEndDate(event.target.value);
-  };
-
-  const handleReleaseNotesClick = async () => {
-    if (startDate !== "" && endDate !== "") {
+  useEffect(() => {
+    const handleReleaseNotesFetch = async () => {
       try {
-        const formattedStartDate = new Date(startDate)
-          .toISOString()
-          .split("T")[0];
-        const formattedEndDate = new Date(endDate).toISOString().split("T")[0];
-
+        const formattedStartDate = startDate ? formatDate(new Date(startDate)) : '';
+        const formattedEndDate = endDate ? formatDate(new Date(endDate)) : '';
+    
         const response = await axios.get(
           "https://localhost:7243/api/OpenAI/responses",
           {
@@ -34,51 +26,44 @@ const ReleaseNotes: React.FC = () => {
             },
           }
         );
-        if (response.data && Array.isArray(response.data)) {
-          const filteredResponse = response.data.find(
-            (item: any) =>
-              item.startDate.startsWith(formattedStartDate) &&
-              item.endDate.startsWith(formattedEndDate)
-          );
+    
+        console.log("Fetched release notes: ", response.data);
+    
+        if (response.data && response.data.length > 0) {
+          const filteredResponse = response.data[0];
           if (filteredResponse) {
             const sections = filteredResponse.responseText.split('\n\n');
             setSections(sections);
           } else {
             setSections([]);
           }
+        } else {
+          setSections([]);
         }
       } catch (error) {
         console.log("Error fetching data:", error);
       }
+    };
+
+    if (startDate !== "" && endDate !== "") {
+      handleReleaseNotesFetch();
     }
-  };
+  }, [startDate, endDate]);
+
+  console.log("Sections: ", sections);
+  console.log("Sections length: ", sections.length);
 
   return (
-    <div className=" text-gray-500">
+    <div className="text-gray-500">
       <h2 className="text-xl font-semibold text-gray-600" style={{ fontFamily: 'Rasa, sans-serif' }}>Release Notes</h2>
-      <input
-        type="date"
-        value={startDate}
-        onChange={handleStartDateChange}
-        className="py-2 px-4 mt-2 rounded-lg border border-gray-200 focus:outline-none focus:border-blue-400"
-      />
-      <input
-        type="date"
-        value={endDate}
-        onChange={handleEndDateChange}
-        className="py-2 px-4 mt-2 ml-2 rounded-lg border border-gray-200 focus:outline-none focus:border-blue-400"
-      />
-      <button
-        onClick={handleReleaseNotesClick}
-        className="py-2 px-4 mt-2 ml-2 bg-gray-200 hover:bg-gray-400  text-slate-500 rounded-lg focus:outline-none"
-      >
-        Get Release Notes
-      </button>
       <div className="prose max-w-none mt-4">
-        {sections.length > 0 ? sections.map((section, index) => (
-          <ReactMarkdown key={index} children={section} />
-        )) : <p></p>
-        }
+        {sections.length > 0 ? (
+          sections.map((section, index) => (
+            <ReactMarkdown key={index} children={section} />
+          ))
+        ) : (
+          <p>No release notes for the selected dates.</p>
+        )}
       </div>
     </div>
   );
